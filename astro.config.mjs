52755f2370efb7ec4d8a54715dc7f55c83f92a6c
OLIMINATOR <oliminator@protonmail.com>
2022-12-04 14:46:22 +0100

    stvorenie veci

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image(), sitemap(), compress(), prefetch()]
+});
