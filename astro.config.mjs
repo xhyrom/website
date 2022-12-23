90d5930f9235d5659769f837d5d13067ec51ae49
OLIMINATOR <oliminator@protonmail.com>
2022-12-23 14:08:00 +0100

    asi hotove

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -17,5 +17,6 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), sitemap(), compress(), prefetch()]
+    site: "https://stargazers.club", //prepisat uwu
+    integrations: [tailwind(), image(), sitemap(), compress(), prefetch()],
 });
