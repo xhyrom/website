ab51d7f40d8757e939c53c04e737a4d54b554f58
xHyroM <generalkubo@gmail.com>
2023-06-04 20:38:44 +0200

    fix(components/widgets/docs): remove marker from heading

--- docs/config.ts
+++ docs/config.ts
@@ -4,28 +4,33 @@ export interface Docs {
 
 export interface SidebarItem {
   text: string;
-  header?: boolean;
-  link?: string;
+  items: SidebarItem[] & {
+    link: string;
+  }[]
 }
 
 export const docs: Docs = {
   sidebar: [
-    { text: "Discord Experiments API", header: true },
     {
-      text: "Introduction",
-      link: "/docs/discord-experiments-api/introduction",
-    },
-    {
-      text: "Experiments",
-      link: "/docs/discord-experiments-api/experiments",
-    },
-    {
-      text: "Eligible",
-      link: "/docs/discord-experiments-api/eligible",
-    },
-    {
-      text: "Stats",
-      link: "/docs/discord-experiments-api/stats",
+      text: "Discord Experiments API",
+      items: [
+        {
+          text: "Introduction",
+          link: "/docs/discord-experiments-api/introduction",
+        },
+        {
+          text: "Experiments",
+          link: "/docs/discord-experiments-api/experiments",
+        },
+        {
+          text: "Eligible",
+          link: "/docs/discord-experiments-api/eligible",
+        },
+        {
+          text: "Stats",
+          link: "/docs/discord-experiments-api/stats",
+        },
+      ],
     },
   ],
 };
