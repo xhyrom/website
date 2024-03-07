b5cb879f48ae8eac506d64a8211aa841e48d9482
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2024-03-07 11:21:37 +0100

    build: bump deps

--- astro.config.ts
+++ astro.config.ts
@@ -13,23 +13,23 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+import icon from "astro-icon";
 import minify from "./astro-integrations/minify";
 
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://xhyrom.dev/",
   integrations: [
     tailwind(),
     sitemap(),
     compress({
-      svg: false,
-      html: false,
+      SVG: false,
+      HTML: false,
     }),
     robotsTxt({
       policy: [
@@ -41,6 +41,7 @@ export default defineConfig({
     }),
     minify(),
     preact(),
+    icon(),
   ],
   vite: {
     resolve: {
